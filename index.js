b9f0ce40d6614b6e76be3e9b27865dcd2b7a79b7
FireStreaker2 <michaelccc2019@outlook.com>
2023-10-29 09:30:00 -0700

    better styles

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
+const show = document.getElementById("show");
+const dataContainer = document.getElementById("data-container");
+const hiddenData = document.getElementById("hidden-data");
+
 document.addEventListener("DOMContentLoaded", () => {
 	chrome.storage.sync.get({ data: {} }, (result) => {
 		const data = result.data || {};
-		const dataContainer = document.getElementById("data-container");
 
 		dataContainer.innerHTML = "";
+		hiddenData.innerHTML = "";
+
+		let count = 1;
 
 		for (const key in data) {
 			const value = data[key];
 			const item = document.createElement("div");
-			item.textContent = `${key}: ${value}`;
-			dataContainer.appendChild(item);
+			item.textContent = `${count}. ${key}: ${value}`;
+			count++;
+
+			count <= 4
+				? dataContainer.appendChild(item)
+				: hiddenData.appendChild(item);
+
+			if (hiddenData.innerHTML !== "") show.style.display = "flex";
 		}
 	});
+});
+
+document.getElementById("reset").addEventListener("click", () => {
+	dataContainer.innerHTML = "";
+	hiddenData.innerHTML = "";
+
+	let data = {};
+	chrome.storage.sync.set({ data }).then(() => {
+		console.log("[INFO] Stats Updated");
+	});
+});
+
+show.addEventListener("click", () => {
+	show.style.display = "none";
+	hiddenData.style.display = "block";
 });
